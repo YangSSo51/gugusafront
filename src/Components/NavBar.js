60894c1916065faefe07db244389fe02e4450f3c
YangSSo51 <48993188+YangSSo51@users.noreply.github.com>
2019-12-26 17:15:04 +0900

    conflict

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React,{Component} from "react";
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
@@ -15,22 +14,4 @@ export default class NavBar extends React.PureComponent{
             </div>
         )
     }
-=======
-import React,{Component} from "react";
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-
-export default class NavBar extends React.PureComponent{
-    render() {
-        return(
-            <div className="header">
-                <Grid container spacing={3}>
-                    <Grid item xs={12}>
-                        <Paper>hello</Paper>
-                    </Grid>
-                </Grid>
-            </div>
-        )
-    }
->>>>>>> 31d9bda8cb3c3d704fab55b17f8589fe236d45df
 }
