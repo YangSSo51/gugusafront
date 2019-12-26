9ada5a206cfb3c973c62529675b46bab0db117c5
puppy9207 <puppy9207@naver.com>
2019-12-26 23:12:17 +0900

    api 싲가

--- src/App.js
+++ src/App.js
@@ -3,21 +3,17 @@ import './App.css';
 import { SignUp,SignIn,Project,input,Detail } from './Components';
 import { Route,BrowserRouter } from 'react-router-dom';
 import NavBar from "./Components/NavBar";
-import Container from '@material-ui/core/Container';
 
 class App extends Component {
     render() {
         return (
             <div className="font">
-                <NavBar/>
                   <BrowserRouter>
                     <NavBar/>
                     <Route exact path="/signup" component={SignUp}/>
                     <Route exact path="/signin" component={SignIn}/>
                     <Route exact path="/detail" component={Detail}/>
-                    <Container maxWidth="lg">
                     <Route exact path="/project" component={Project}/>
-                    </Container>
                     <Route exact path="/input" component={input}/>
                   </BrowserRouter>
             </div>
